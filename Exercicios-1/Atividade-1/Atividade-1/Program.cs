6c54e1e5b182fbe43b80f79d3b77983d36e25aeb
Liuizn <Luizgabrielsnn@gmail.com>
2021-05-05 19:05:39 -0300

    questão 1 feita

---
+++ Exercicios-1/Atividade-1/Atividade-1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um valor: ");
+            int num1 = int.Parse (Console.ReadLine());
+
+            Console.Write("Digite outro valor: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+
+            int soma = num1 + num2;
+
+            Console.WriteLine($"O valor da soma é {soma}");
+
+            Console.ReadLine();
+        }
+    }
+}
