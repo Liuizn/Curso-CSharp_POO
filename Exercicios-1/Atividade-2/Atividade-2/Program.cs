0caeeba3f1fc1d8ebdf6ad190401f1e374d20db7
Liuizn <Luizgabrielsnn@gmail.com>
2021-05-05 19:22:48 -0300

    atividade 2 e 3

---
+++ Exercicios-1/Atividade-2/Atividade-2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Digite o valor do raio: ");
+            Console.Write("Digite o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(raio);
+
+            double pi = 3.14159;
+
+            double area = pi * Math.Pow(raio, 2);
+
+
+            Console.WriteLine($"O valor da Área é {area}");
+
+           Console.ReadLine();
+        }
+    }
+}
---
+++ Exercicios-1/Atividade-3/Atividade-3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Atividade_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira o 1 número: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Insira o 2 número: ");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.Write("Insira o 3 número: ");
+            int num3 = int.Parse(Console.ReadLine());
+            Console.Write("Insira o 4 número: ");
+            int num4 = int.Parse(Console.ReadLine());
+
+            int diference = ((num1 * num2) - (num3 * num4));
+
+            Console.WriteLine($"A diferença entre os valores é {diference}");
+        }
+    }
+}
+
