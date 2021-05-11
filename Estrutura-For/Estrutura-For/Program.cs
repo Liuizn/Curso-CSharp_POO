426087ba8e81aeb83e660735ac5ec627f4f4a519
Liuizn <Luizgabrielsnn@gmail.com>
2021-05-11 19:05:54 -0300

    aula sobre for

---
+++ Estrutura-For/Estrutura-For/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estrutura_For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira quantos números você vai digitar: ");
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= N; i++)
+            {
+                Console.WriteLine("oi");
+            }
+        }
+    }
+}
