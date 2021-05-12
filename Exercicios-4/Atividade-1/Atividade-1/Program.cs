41b4defc0d81c1d440c447e239b7fa6ee03d68ff
Liuizn <Luizgabrielsnn@gmail.com>
2021-05-12 20:46:27 -0300

    adicionando folder fixação

---
+++ Exercicios-4/Atividade-1/Atividade-1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Insira um valor: ");
+            int valor = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= valor; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
