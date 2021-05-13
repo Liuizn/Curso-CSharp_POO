d98548e1d710a1e03f55eb6761fb81a5288b2995
Liuizn <Luizgabrielsnn@gmail.com>
2021-05-13 19:45:11 -0300

    Atividades aulas 13/05/2021

---
+++ Exercicios-4/Atividade-2/Atividade-2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira o valor de números: ");
+            int N = int.Parse(Console.ReadLine());
+            int dentro = 0;
+            int fora = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write("Insira o valor: ");
+                int num = int.Parse(Console.ReadLine());
+
+                if (num >= 10 && num <= 20)
+                {
+                    dentro += 1;
+                } 
+                else
+                {
+                    fora += 1;
+                }
+            }
+
+            Console.WriteLine($"{dentro} in");
+            Console.WriteLine($"{fora} out");
+        }
+    }
+}
---
+++ Exercicios-4/Atividade-3/Atividade-3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos testes?: ");
+            int numeroTestes = int.Parse(Console.ReadLine());
+
+            double num1;double num2; double num3; double saida;
+
+            for (int i = 0; i < numeroTestes; i++)
+            {
+                Console.Write("Insira o primeiro número: ");
+                num1 = double.Parse(Console.ReadLine());
+
+                Console.Write("Insira o segundo número: ");
+                num2 = double.Parse(Console.ReadLine());
+
+                Console.Write("Insira o terceiro número: ");
+                num3 = double.Parse(Console.ReadLine());
+
+
+                saida = ((num1 * 2) + (num2 * 3) + (num3 * 5)) / (2 + 3 + 5);
+
+                Console.WriteLine($"Saída: {saida:F1}");
+
+
+            }
+               
+        }
+    }
+}
