e146b19adb7d2f6450d065666944581652f490d8
Liuizn <Luizgabrielsnn@gmail.com>
2021-05-14 21:45:17 -0300

    começando Orientação a Objetos

---
+++ resolvendo-problema/resolvendo-problema/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace resolvendo_problema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double xA, xB, xC, yA, yB, yC;
+
+            Console.WriteLine("Entre com as medidas do triângulo X: ");
+            xA = double.Parse(Console.ReadLine());
+            xB = double.Parse(Console.ReadLine());
+            xC = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Entre com as medidas do triângulo Y: ");
+            yA = double.Parse(Console.ReadLine());
+            yB = double.Parse(Console.ReadLine());
+            yC = double.Parse(Console.ReadLine());
+
+            double p = (xA + xB + xC) / 2.0;
+            double areaX = Math.Sqrt(p * (p - xA) * (p - xB) * (p - xC));
+
+             p = (yA + yB + yC) / 2.0;
+            double areaY = Math.Sqrt(p * (p - yA) * (p - yB) * (p - yC));
+
+            Console.WriteLine($"A área de X é: {areaX.ToString("F4")}");
+            Console.WriteLine($"A área de Y é: {areaY.ToString("F4")}");
+
+        }
+    }
+}
---
+++ resolvendo-problema/resolvendo-problema/Triangulo.cs
@@ -0,0 +1,10 @@
+﻿namespace resolvendo_problema
+{
+    class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+
+    }
+}
