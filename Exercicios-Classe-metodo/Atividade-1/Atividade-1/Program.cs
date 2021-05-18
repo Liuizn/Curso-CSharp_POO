02d726bc8380bb8ce88bdd1328adc72dbb66ff51
Liuizn <Luizgabrielsnn@gmail.com>
2021-05-18 19:55:52 -0300

    Atividade-1

--- Exercicios-Classe-metodo/Atividade-1/Atividade-1/Program.cs
+++ Exercicios-Classe-metodo/Atividade-1/Atividade-1/Program.cs
@@ -11,6 +11,21 @@ namespace Atividade_1
         static void Main(string[] args)
         {
             //Criação da Classe
+            Triangulo triang = new Triangulo();
+
+            //Entrada de Dados do Triangulo
+            Console.WriteLine("Insira a Largura do Triângulo: ");
+            triang.Largura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira a Altura do Triângulo: ");
+            triang.Altura = double.Parse(Console.ReadLine());
+            //Fim Entrada
+
+            //Saída dos Dados
+            Console.WriteLine($"AREA = {triang.CalcularArea():F2}");
+            Console.WriteLine($"PERIMETRO = {triang.CalcularPerimetro():F2}");
+            Console.WriteLine($"DIAGONAL = {triang.CalcularDiagonal():F2}");
+
         }
     }
 }
--- Exercicios-Classe-metodo/Atividade-1/Atividade-1/Triangulo.cs
+++ Exercicios-Classe-metodo/Atividade-1/Atividade-1/Triangulo.cs
@@ -7,5 +7,22 @@ namespace Atividade_1
     {
         public double Largura;
         public double Altura;
+
+        public double CalcularArea()
+        {
+            return Largura * Altura;
+        }
+
+        public double CalcularPerimetro()
+        {
+            return 2 * (Largura + Altura);
+        }
+
+        public double CalcularDiagonal()
+        {
+            return Math.Sqrt(Altura * Altura + Largura * Largura);
+        }
+
+
     }
 }
