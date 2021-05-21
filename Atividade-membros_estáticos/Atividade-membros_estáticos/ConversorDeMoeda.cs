f6a03cf5319cd65372017c6b5db1efb90480f013
Liuizn <Luizgabrielsnn@gmail.com>
2021-05-20 23:43:47 -0300

    atividade de fixação

---
+++ Atividade-membros_estáticos/Atividade-membros_estáticos/ConversorDeMoeda.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace Atividade_membros_estáticos
+{
+    class ConversorDeMoeda
+    {       
+        public static double ConversorDolarToReal(double qtd, double cot)
+        {
+            return (qtd * cot) * 1.06;
+        }
+    }
+}
---
+++ Atividade-membros_estáticos/Atividade-membros_estáticos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_membros_estáticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira a Cotação do dólar: ");
+            double CotDolar = double.Parse(Console.ReadLine());
+
+            Console.Write("Quantos dólares você irá comprar: ");
+            double QtdDolar = double.Parse(Console.ReadLine());
+
+            double ValorConvertido = ConversorDeMoeda.ConversorDolarToReal(QtdDolar, CotDolar);
+
+            Console.WriteLine($"Valor a ser pago em reais: {ValorConvertido:F2}");
+        }
+    }
+
+}
