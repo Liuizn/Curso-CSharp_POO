d70a9e38b2bd828e554d8b92be29731d8b5b13e4
Liuizn <Luizgabrielsnn@gmail.com>
2021-08-09 19:05:42 -0300

    Structs

---
+++ Structs/Structs/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p;
+            p.X = 302;
+            p.Y = 444;
+
+            Console.WriteLine(p);
+
+
+
+        }
+    }
+}
