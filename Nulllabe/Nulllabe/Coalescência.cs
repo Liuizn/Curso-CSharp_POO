4df7fdc869fdf9c7f86a8989ec49415270d96955
Liuizn <Luizgabrielsnn@gmail.com>
2021-08-11 07:35:22 -0300

    Nullabe

---
+++ Nulllabe/Nulllabe/Coalescência.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Class1
+{
+    public Class1()
+    {
+        double? x = null;
+        double? y = 10;
+
+        double a = x ?? 5;
+        double b = y ?? 5;
+
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+
+    }
+}
---
+++ Nulllabe/Nulllabe/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nulllabe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double? x = null;
+            double? y = 10.0;
+
+            Console.WriteLine(x.GetValueOrDefault());
+            Console.WriteLine(y.GetValueOrDefault());
+            
+
+            Console.WriteLine(x.HasValue);
+            Console.WriteLine(y.HasValue);
+
+            Console.WriteLine(x.Value);
+        }
+    }
+}
