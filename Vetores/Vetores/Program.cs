083d2db12b4e80cf9fb4de57a105a448b3bfbe43
Liuizn <Luizgabrielsnn@gmail.com>
2021-08-11 15:39:40 -0300

    atividade fixação

---
+++ Vetores/Vetores/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            double[] vetor = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                vetor[i] = double.Parse(Console.ReadLine());
+            }
+
+        }
+    }
+}
